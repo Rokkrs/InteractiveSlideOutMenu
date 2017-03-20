6fb87042e060c9e23ef1a04dfab9edaf7d3db013
Rokkrs <oscar.cuadra.navarro@gmail.com>
2017-03-20 13:02:22 -0300

    Add files via upload
    Version for swift 3

---
+++ InteractiveSlideOutMenu-Rokkrs/InteractiveSlideoutMenu/Interactor.swift
@@ -0,0 +1,15 @@
+//
+//  Interactor.swift
+//  InteractiveSlideoutMenu
+//
+//  Created by Oscar on 3/17/17.
+//  Copyright © 2017 Thorn Technologies, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Interactor: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
